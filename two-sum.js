ac56e4e39ce87f886aeaa0d70ff75ea0a16e06bd
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-10-07 10:12:35 -0700

    actual indices found

--- two-sum.js
+++ two-sum.js
@@ -10,7 +10,7 @@
 
 var twoSum = function (nums, target) {
     for (var i = 0; i < nums.length - 1; i++) {
-        for (var j = 1; j < nums.length; j++) {
+        for (var j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j];
             }
